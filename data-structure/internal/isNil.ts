ccb5de47937b64605e6e7d00d72166376cbf6368
JNMarlon <wonjin6368@naver.com>
2024-06-12 22:50:17 +0900

    remove unused code 2

--- data-structure/internal/isNil.ts
+++
@@ -1,9 +0,0 @@
-type Nullable<T> = T extends null | undefined ? T : never;
-
-export const isNil = <T>(a: T): a is Nullable<T> => {
-    if (a === undefined || a === null) {
-        return true;
-    }
-
-    return false;
-};
--- data-structure/internal/util.ts
+++
@@ -1,6 +0,0 @@
-export const defaultToString = (item: any): string => {
-    if (item === null) return 'NULL';
-    if (item === undefined) return 'UNDEFINED';
-    if (typeof item === 'string' || item instanceof String) return `${item}`;
-    return item.toString();
-};
